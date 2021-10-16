16ddd71c2cb2f6600a8a148b4129ea9ef4e7101a
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-16 12:36:39 +0530

    button added

--- Tkinter/zoom_pdf_reader.py
+++ Tkinter/zoom_pdf_reader.py
@@ -1,6 +1,12 @@
 from tkinter import *
-from tkPDFViewer import tkPDFViewer as pdf
+from tkinter import filedialog 
+from tkPDFViewer import tkPDFViewer as PDF
 
 root = Tk();
-root.geometry("500x500")
+root.geometry("630x700+400+100");
+root.title("PDF_READER");
+root.configure(bg="gray");
+
+
+Button(root,text="Open a PDF file " , width = 80 , font = " aerial 20",bd= 4).pack();
 root.mainloop();
