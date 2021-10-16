60d31dd07d1f87a01b127eee6e778051261a7f9d
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-16 21:17:28 +0530

    pdftotxt_converter

--- Tkinter/pdf_to_txt.py
+++ Tkinter/pdf_to_txt.py
@@ -1,13 +1,8 @@
-import PyPDF2
-pdf = open("/home/atmegarobotics/Desktop/app_development/Tkinter/Base.pdf","rb");
 
-pdf1 = PyPDF2.PdfFileReader(pdf);
-
-x = int(input("enter the pages you want to extract :",));
-pdf2 = pdf1.getPage(x);
-
-text = pdf2.extractText();
-
-file1 = open(r"/home/atmegarobotics/Desktop/app_development/Tkinter/text.txt","a");
-file1.writelines(text)
-print("extracted successfully");
+import pdftotext
+with open("/home/atmegarobotics/Desktop/app_development/Tkinter/Base.pdf", "rb") as f:
+    pdf = pdftotext.PDF(f)
+ 
+# Save all text to a txt file.
+with open('/home/atmegarobotics/Desktop/app_development/Tkinter/text.txt', 'w') as f:
+    f.write("\n\n".join(pdf))
