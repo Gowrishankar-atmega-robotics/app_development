705b139a243826a680852768456ae7c7c9b6ccd8
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-17 12:16:40 +0530

    pdf_to_text

--- Tkinter/pdf_to_txt.py
+++ Tkinter/pdf_to_txt.py
@@ -1,8 +1,16 @@
-
+from tkinter import *
+from tkPDFViewer import tkPDFViewer as pdf
+from tkinter import filedialog
 import pdftotext
-with open("/home/atmegarobotics/Desktop/app_development/Tkinter/Base.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
- 
-# Save all text to a txt file.
-with open('/home/atmegarobotics/Desktop/app_development/Tkinter/text.txt', 'w') as f:
-    f.write("\n\n".join(pdf))
+
+root = Tk()
+root.geometry("500x500");
+root.title("PDFtoTEXT")
+def BF():
+    filename = filedialog.askopenfilename(title="Select pdf",filetypes=[("text files","*.pdf")])
+    v1 = open(filename,"rb")
+    pd = pdftotext.PDF(v1)
+    v2 = open('/home/atmegarobotics/Desktop/app_development/Tkinter/text.txt','w')
+    v2.write("".join(pd))
+Button(root,text="PDF to TexT",command=BF , width= 100,height=1 , font= "Aerial 20" , bd=6).pack();
+root.mainloop()
