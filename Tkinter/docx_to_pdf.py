e2c7412f8d212ad79179f4ffea1b94092bf14a62
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-17 21:13:23 +0530

    doxctopdf

--- Tkinter/docx_to_pdf.py
+++ Tkinter/docx_to_pdf.py
@@ -4,3 +4,17 @@ from tkinter import filedialog
 from tkinter.ttk as ttk 
 from tkinter.messagebox import showinfo
 
+root = Tk();
+root.title("word to pdf");
+def openfile():
+    filename = filedialog(filetype=[("docx","*.docx")]);
+    convert(file.name,'/convert.pdf')
+    showinfo("Done","File Succesfully Converted")
+
+label = tk.Label(root, text= "Choose File:- ")
+label.grid(row=0, column=0)
+
+button = ttk.Button(root, text="Select", width=30, command=openfile)
+button.grid(row=0, column=1)
+
+root.mainloop()
