664fbae1a1d99fb2afa42cd181c5a8f0805af7fc
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-17 21:48:03 +0530

    music_player

--- Tkinter/play_sound.py
+++ Tkinter/play_sound.py
@@ -1,2 +1,9 @@
 from playsound import playsound
-playsound("/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3")
+from tkinter import *
+root = Tk();
+root.geometry("250x250");
+root.title("musicplayer")
+def playsong():
+    playsound("/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3");
+Button(root,text="play",width=100,height=1,command=playsong );
+root.mainloop()
