f3c2f68aa05fd26663662b16d622606e528ff826
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-17 20:58:05 +0530

    odt_to_pdf

--- Tkinter/odt_To_pdf.py
+++ Tkinter/odt_To_pdf.py
@@ -1,8 +1,14 @@
 from tkinter import *
 from tkPDFViewer import tkPDFViewer as pdf
 import convertapi
-
+from tkinter import filedialog
 root = Tk();
 root.geometry("500x500");
 root.title("ODT_to_PDF_Converter");
+def files():
+    filename=filedialog.askopenfilename(filetypes=[("all files","*.*")])
+    convertapi.api_secret = 'VBU2YahJGZ4FM8PX'
+    convertapi.convert('pdf',{'File': filename }, from_format = 'odt').save_files('/home/atmegarobotics/Desktop/app_development/Tkinter/')
+    
+Button(root,text="Open files",command= files ,width=100 , height=1, bd=6).pack();
 root.mainloop()
