04a341589c8227d8088cf3b7505e7830c1e1b506
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-17 14:40:48 +0530

    demo_odtTopdf_1

--- Tkinter/demo_odtTopdf.py
+++ Tkinter/demo_odtTopdf.py
@@ -1,12 +1,4 @@
 import convertapi
-
-
 convertapi.api_secret = 'VBU2YahJGZ4FM8PX'
 convertapi.convert('pdf',{ 'File': '/home/atmegarobotics/Desktop/demo.odt'}, from_format = 'odt').save_files('/home/atmegarobotics/Desktop/app_development/Tkinter/')
 
-"""
-convertapi.api_secret = 'VBU2YahJGZ4FM8PX'
-convertapi.convert('pdf', {
-    'File': '/home/atmegarobotics/Desktop/demo.odt'
-}, from_format = 'odt').save_files('/home/atmegarobotics/Desktop/app_development/Tkinter/')
-"""
