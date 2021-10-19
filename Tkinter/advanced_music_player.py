8e413bd943d58b6ea0b655ace2476c3190fce889
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-19 20:18:11 +0530

    song_stopper

--- Tkinter/advanced_music_player.py
+++ Tkinter/advanced_music_player.py
@@ -1,11 +1,12 @@
 from tkinter import *
+from tkinter import filedialog
+from pygame import *
 root = Tk();
 root.geometry("1080x607");
 root.title("MusicPlayer");
 img = PhotoImage(file="/home/atmegarobotics/Desktop/app_development/Tkinter/bg.png");
 start_icon = PhotoImage(file='/home/atmegarobotics/Desktop/app_development/Tkinter/start.png');
 pause_icon = PhotoImage(file='/home/atmegarobotics/Desktop/app_development/Tkinter/pause.png');
-
 bgl = Label(root,image=img);
 bgl.place(x=0,y=0,relwidth=1,relheight=1);
 Button(root,image=start_icon,bd = 6).place(x=550,y=470,width=64,height=64);
---
+++ Tkinter/simple_song_stopper.py
@@ -0,0 +1,12 @@
+#from tkinter import *
+import playsound as playsound
+
+"""
+root = Tk();
+root.title("musicplayer");
+root.geometry("500x500");
+def play():
+    playsound('/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3');
+Button(root,text="play").pack();
+
+"""
