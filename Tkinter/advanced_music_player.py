96b36ca7eda79d2fa43b6050b147b4e2e8181671
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-19 23:18:52 +0530

    added_play_pause

--- Tkinter/advanced_music_player.py
+++ Tkinter/advanced_music_player.py
@@ -2,13 +2,27 @@ from tkinter import *
 from tkinter import filedialog
 from pygame import *
 root = Tk();
+mixer.init();
+
+def play():
+    global filename ;
+    #filename = mixer.music.load('/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3');
+    filename = mixer.music.load(filedialog.askopenfilename(filetypes=[("files types","*.mp3")]))
+    mixer.music.play()
+def stop():
+    mixer.music.stop();
+def pause():
+    mixer.music.pause();
 root.geometry("1080x607");
 root.title("MusicPlayer");
+
 img = PhotoImage(file="/home/atmegarobotics/Desktop/app_development/Tkinter/bg.png");
 start_icon = PhotoImage(file='/home/atmegarobotics/Desktop/app_development/Tkinter/start.png');
 pause_icon = PhotoImage(file='/home/atmegarobotics/Desktop/app_development/Tkinter/pause.png');
+stop_icon = PhotoImage(file="");
 bgl = Label(root,image=img);
 bgl.place(x=0,y=0,relwidth=1,relheight=1);
-Button(root,image=start_icon,bd = 6).place(x=550,y=470,width=64,height=64);
-Button(root,image=pause_icon,bd = 6).place(x=482,y=470,width=64,height=64);
+Button(root,image=start_icon,command = play , bd = 6).place(x=400,y=470,width=64,height=64);
+Button(root,image=pause_icon,command = stop , bd = 6).place(x=500,y=470,width=64,height=64);
+Button(root,text="stop",bd =6 ).place(x=600,y=470,width=64, height=64)
 root.mainloop();
--- Tkinter/simple_song_stopper.py
+++ Tkinter/simple_song_stopper.py
@@ -1,12 +1,16 @@
-#from tkinter import *
-import playsound as playsound
-
-"""
+from tkinter import *
+#import playsound as playsound
 root = Tk();
 root.title("musicplayer");
 root.geometry("500x500");
-def play():
-    playsound('/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3');
-Button(root,text="play").pack();
-
-"""
+listbox = Listbox(root,width=10,height=10,bg="#6b6b6b");
+listbox.insert(1,"python");
+listbox.insert(2,"c++");
+listbox.insert(3,"panda");
+listbox.insert(4,"conda");
+listbox.insert(5,"c");
+listbox.pack();
+#def play():
+#    playsound('/home/atmegarobotics/Desktop/app_development/Tkinter/aud.mp3');
+#Button(root,text="play").pack();
+root.mainloop();
