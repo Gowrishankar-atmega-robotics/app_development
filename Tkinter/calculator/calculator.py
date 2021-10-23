c196742691f5aeb921d2c6c5ac530aa4c56f56d6
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-23 21:17:06 +0530

    calculator

--- Tkinter/calculator/calculator.py
+++ Tkinter/calculator/calculator.py
@@ -1,5 +1,61 @@
+
 from tkinter import *
-root =  Tk();
-root.title("calculator");
-root.geometry("500x500")
-root.mainloop();
+from functools import partial 
+from tkinter import messagebox
+import math
+root= Tk()
+root.geometry("400x400")
+root.title("Calculator")
+def AddNum(X,Y):
+    A = X.get()
+    B = Y.get()
+    C= int(A)+int(B)
+    print("Sum =",C)
+    #Entry(root, text = C ).pack()
+    messagebox.showinfo("sum", C)
+    return
+def SubNum(X,Y):
+    a = X.get();
+    b = Y.get();
+    c = float(a)- float(b);
+    messagebox.showinfo("difference", c)
+def MultiNum():
+    a = X.get();
+    b = Y.get();
+    c = float(a)*float(b);
+    messagebox.showinfo("mutliple", c);
+def DiviNum():
+    a = X.get();
+    b = Y.get();
+    c = float(a)/float(b)
+    messagebox.showinfo("divided",c)
+def Square():
+    a = X.get();
+    c = int(a)
+    e = math.sqrt(c);
+    messagebox.showinfo("Squareroot",e)
+def percentage():
+    a = X.get()
+    b = Y.get()
+    c = (int(a)/int(b))*100
+    messagebox.showinfo("percentage",c);
+def expo():
+    a = X.get();
+    b = Y.get();
+
+    c = int(a)**int(b) ;
+    messagebox.showinfo("expo",c)
+X = StringVar()
+Y = StringVar()
+entr1 = Entry(root,textvariable=X).place(x = 0 , y = 0 , width = 200 , height = 200)
+entr2 = Entry(root,textvariable=Y).place(x= 200 , y = 0 , width = 200 , height= 200)
+Button(root, text="+", command = AddNum).place(x=0 , y = 200 ,width= 100, height = 100);
+Button(root, text = "-", command = SubNum).place(x = 0, y = 300, width = 100, height = 100);
+Button(root, text = "x", command = MultiNum ).place( x = 100 , y = 200 , width = 100, height= 100);
+Button(root, text = "/", command = DiviNum ).place(x = 100 , y = 300 , width = 100 , height = 100) 
+Button(root, text ="SquareRoot", command = Square).place(x = 200, y = 200 , width = 100 , height = 100);
+Button(root, text ="percentage", command = percentage).place(x= 200 , y = 300, width = 100, height = 100)
+Button(root, text ="expoential",command = expo ).place(x = 300 , y = 200, width = 100 , height = 100);
+Button(root, text ="Clear").place(x = 300 , y = 300 , width = 100 , height = 100)
+root.mainloop()
+
---
+++ Tkinter/calculator/demo.py
@@ -0,0 +1,48 @@
+"""
+
+**********************************it's code not completed will update soon***************************************
+
+
+from tkinter import *
+from functools import partial
+root = Tk();
+root.geometry("500x500");
+
+def one():
+    global X ;
+    1 = str("1");
+    
+    Entry(root, textvariable= 1).place(x = 0,y = 40, width = 250 , height= 200 )
+def two():
+    global Y ;
+    2  = str("2");
+    Entry(root, textvariable= 2 ).place(x = 250 , y = 40 , width = 250 , height= 200)
+#X = StringVar();
+#Y = StringVar();
+def addnum():
+    global a , b ;
+    a = X.get();
+    b = Y.get();
+    c = int(a)+int(b);
+    print("sum = ", c);
+    return 
+Button(root, text = " advanced setting" ).place(x = 0 , y = 0 , width = 500 , height = 45 )
+Entry(root,textvariable = X ).place(x = 0 , y = 40, width=250,height=200 );
+Entry(root, textvariable= Y ).place(x = 250 , y = 40 , width =250 , height=200);
+#AddNum= partial(addnum, X,Y)
+bt1 = Button(root, text="Add", command = addnum).pack()
+Button(root, text = "1" , command = one ).place(x = 0 , y = 420,width=90 , height=90);
+Button(root, text ="2" , command = two).place(x = 90, y = 420 , width= 90 , height=90);
+Button(root, text = "3").place(x = 180 , y = 420 , width = 90 , height = 90);
+Button(root,text="4").place(x = 0 , y = 330 , width = 90 , height = 90);
+Button(root,text="5").place(x = 90 , y = 330 , width = 90 , height= 90);
+Button(root,text="6").place(x = 180, y = 330 , width = 90 , height= 90);
+Button(root,text="7").place(x = 0 , y = 240 , width = 90 , height = 90);
+Button(root,text="8").place(x = 90 , y = 240 , width = 90 , height = 90);
+Button(root, text="9").place(x = 180 , y = 240 , width = 90 , height = 90);
+#Button(root,text="10").pack()
+
+root.mainloop()
+
+********************************************not completed**************************************************
+"""
