634aab84573126e64c018d6c600cd6342fe78dea
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-30 10:52:42 +0530

    enterprise-application-2

--- entereprise-application/enterprise-application.py
+++ entereprise-application/enterprise-application.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import mainloop, messagebox
 import random
 root = tk.Tk()
 root.title("Billing Management System");
 root.geometry('1250x720');
 bg_color = '#4D0039'
+title = tk.Label(root , text = "Billing software", bd=12,bg=bg_color,fg='white',font=('times new roman',25,'bold')).pack()
+#root.mainloop()
 root.mainloop()
