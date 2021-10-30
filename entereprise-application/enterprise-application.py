5ddd92e3c4cb98ef51a978e09e9b48af4fe0b991
Gowrishankar P <atmegarobotics@gmail.com>
2021-10-30 10:38:18 +0530

    enterprise-apllication-0

---
+++ entereprise-application/enterprise-application.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+root = tk.Tk()
+root.title("Billing Management System");
+root.geometry('1250x720');
+bg_color = '#4D0039'
+root.mainloop()
